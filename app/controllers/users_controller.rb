1495fc8a222b78effc378eeaaee02b446355ecb1
Кирилл Крылов <kirillmac@MacBook-Air-Kirill.local>
2019-12-03 17:57:53 +0300

    first alpha version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
 
+  # Девайзовский фильтр, который посылает незалогинившихся юзеров
+  # Просматривать профили могут и анонимы
+  before_action :authenticate_user!, except: [:show]
+  # Задаем объект @user для шаблонов и экшенов
+  before_action :set_current_user, except: [:show]
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -17,8 +22,8 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
+  def set_current_user
+    @user = current_user
   end
 
   # Пропишем, что разрешено передавать в params
