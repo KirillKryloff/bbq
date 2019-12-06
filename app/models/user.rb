74b5549c0acc9523e9e673bd58452febdb3544cf
Кирилл Крылов <kirillmac@MacBook-Air-Kirill.local>
2019-12-06 17:17:18 +0300

    comments & subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # Юзер может создавать много событий
   has_many :events
-
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
   # Добавим заодно валидации для юзера
   # Имя не не более 35 символов
   validates :name, presence: true, length: {maximum: 35}
@@ -16,9 +17,15 @@ class User < ApplicationRecord
   # validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = "Пользователь №#{rand(777)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
   end
 end
