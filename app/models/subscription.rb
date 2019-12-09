c4e46379ce2ea4cbd2779ccb513f408f17bb1ef6
Кирилл Крылов <kirillmac@MacBook-Air-Kirill.local>
2019-12-10 00:05:12 +0300

    add subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -11,12 +11,16 @@ class Subscription < ApplicationRecord
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
   # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
 
   # Или один email может использоваться только один раз (если анонимная подписка)
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-  # Если есть юзер, выдаем его имя,
-  # если нет – дергаем исходный метод
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+
+
+  # запрет подписки на свое событие
+  validate :user_not_organizer, on: :create, if: -> { user.present? }
+  validate :unique_email, on: :create, unless: -> { user.present? }
+
   def user_name
     if user.present?
      user.name
@@ -33,5 +37,13 @@ class Subscription < ApplicationRecord
     else
       super
     end
+  end
+
+  def user_not_organizer
+    errors.add(:user, I18n.t('errors.organizer_error')) if user == event.user
+  end
+
+  def unique_email
+    errors.add(:user_email, I18n.t('errors.unique_email')) if User.all.map(&:email).include?(user_email)
   end
 end
